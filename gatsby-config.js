f111a1113ad06f8bda52ea5d9dea4923ffa52b03
tomglasseye <tom@dawnconcept.com>
2020-07-22 14:28:37 +0100

    mobile header theme colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#37384c`,
+        theme_color: `#37384c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
